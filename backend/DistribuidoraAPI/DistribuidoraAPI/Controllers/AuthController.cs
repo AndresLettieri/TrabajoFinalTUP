145d03071a39fe76f9a54c34ea1956cc5164df54
AndresLettieri <andreslettieri@yahoo.com.ar>
2026-08-21 17:22:14 -0300

    Se crean endpoints de autorizacion para login y registro de usuarios

---
+++ backend/DistribuidoraAPI/DistribuidoraAPI/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using DistribuidoraAPI.DTOs.User;
+using DistribuidoraAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistribuidoraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<UserResponseDto>> Register(CreateUserRequest request)
+        {
+            try
+            {
+                var response = await _userService.Create(request);
+                return Ok(response);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserResponseDto>> Login(AuthUserRequest request)
+        {
+            try
+            {
+                var user = await _userService.GetByEmailAndPassword(request.Email, request.Password);
+                if (user is null)
+                {
+                    return Unauthorized(new { message = "Credenciales inválidas" });
+                }
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Ocurrió un error al procesar la solicitud", details = ex.Message });
+            }
+        }
+    }
+}
